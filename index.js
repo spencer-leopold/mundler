d3b0fc683f0b15cacdc4c373b4f2538eb5373625
Spencer Leopold <spencer.leopold@gmail.com>
2015-04-08 02:25:29 -0400

    added functionality for watchify

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ chalk.enabled = true;
 
 function EstrnBrowserify(options) {
   this.files = [];
+  this.externalFiles = [];
+  this.vendorRequires = [];
 
   this.cwd = options.cwd;
   this.app = options.app;
@@ -20,23 +22,127 @@ function EstrnBrowserify(options) {
 
   var self = this;
 
-  this.getVendorFiles(this.vendor);
-  console.log(vendorRequires);
-  if (this.watch) {
-  }
+  async.series([
+    function(callback) {
+      self.getVendorFiles();
+      callback();
+    },
+    function(callback) {
+      self.getMainFiles();
+      callback();
+    },
+    function(callback) {
+      self.buildVendorBundle(self.watch);
+      callback();
+    },
+    function(callback) {
+      self.buildMainBundles(self.watch);
+      callback();
+    }
+  ]);
 }
 
 EstrnBrowserify.prototype.constructor = EstrnBrowserify;
 
-EstrnBrowserify.prototype.getVendorFiles = function(dir) {
-  var vendorRequires = [];
+EstrnBrowserify.prototype.getVendorFiles = function() {
   var self = this;
-  dir = dir + '/*.js';
+  var dir = this.vendor + '/*.js';
 
   glob(dir, {}, function(err, filesArr) {
-    var module = path.basename(filesArr[0], '.js');
-    console.log(module);
-    self.vendorRequires.push('./'+filesArr[0]+':'+module);
+    async.each(filesArr, function(file, next) {
+      var module = path.basename(file, '.js');
+      self.vendorRequires.push({ file: './'+file, expose: module});
+      self.externalFiles.push('./'+file);
+      next();
+    });
+  });
+}
+
+EstrnBrowserify.prototype.getMainFiles = function() {
+  var self = this;
+  var dir = this.app + '/*.js';
+
+  glob(dir, {}, function(err, filesArr) {
+    async.each(filesArr, function(file, next) {
+      var name = path.basename(file, '.js');
+      self.files.push({ file: './'+file, name: name });
+      next();
+    });
+  });
+}
+
+EstrnBrowserify.prototype.buildVendorBundle = function(watch) {
+  var self = this;
+
+  setTimeout(function() {
+    new EstrnBundler(false, self.vendorRequires, false, self.watch);
+  }, 1000);
+}
+
+
+EstrnBrowserify.prototype.buildMainBundles = function(watch) {
+  var self = this;
+
+  setTimeout(function() {
+    async.each(self.files, function(fileObj, next) {
+      new EstrnBundler(fileObj, false, self.externalFiles, self.watch);
+    });
+  }, 1000);
+}
+
+function EstrnBundler(options, requires, external, watch) {
+  if (!watch) {
+    var b = browserify();
+  }
+  else {
+    var b = browserify({ cache: {}, packageCache: {} });
+    b = watchify(b);
+  }
+
+  if (requires.length) {
+    async.series([
+      function(callback) {
+        async.each(requires, function(required, next) {
+          b.require(required.file, { expose: required.expose });
+          next();
+        })
+        callback();
+      },
+      function(callback) {
+        b.bundle(function(err, buf) {
+          fs.writeFile('./test.js', buf);
+          console.log('wrote vendor bundle');
+        });
+        callback();
+      }
+    ]);
+  }
+
+  if (external.length) {
+    b.external(external);
+    b.add(options.file);
+
+    b.bundle(function(err, buf) {
+      fs.writeFile(process.cwd() + '/bundle-'+options.name+'.js', buf);
+      console.log('wrote %s bundle', options.name);
+    });
+
+    b.on('update', function (ids) {
+      console.log(ids);
+      b.bundle();
+      // b.bundle(function(err, buf) {
+      //   fs.writeFile('./bundle-'+options.name+'.js', buf);
+      //   console.log('wrote %s bundle', options.name);
+      // });
+    });
+  }
+
+  b.on('error', function (err) {
+    console.log(err);
+  });
+
+  b.on('time', function (time) {
+    console.log(time);
   });
 }
 
